31b0e02888decce1cf46feeefdded4fc6c79b766
Andrew Hwang <Andrew@Andrews-MBP.attlocal.net>
2018-03-12 10:23:58 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from './components/container';
+
+import store from './redux/store/index'
+import { Provider } from 'react-redux'
+
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.render(
+    <Provider store={store}>
+    <BrowserRouter>
+    <Container />
+    </BrowserRouter></Provider>
+            , document.getElementById('app')
+);
