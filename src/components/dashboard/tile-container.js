a49a7f4adb73340f991b5044a0e84e550e2a2daf
Andrew Hwang <Andrew@Andrews-MBP.attlocal.net>
2018-03-13 11:17:19 -0700

    implemented DRAGULA in tile-container

--- src/components/dashboard/tile-container.js
+++ src/components/dashboard/tile-container.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import dragula from 'react-dragula';
+import ChartTile from './../tiles/chart-tile';
+import ReactDOM from 'react-dom';
+import Dragula from 'react-dragula';
 
 import DashboardTile from './dashboard-tile';
 import LargeTile from './large-tile';
@@ -13,33 +15,46 @@ class TileContainer extends Component {
             img: 'https://www.muralswallpaper.co.uk/app/uploads/Black-and-White-typography-map-maps-plain-kj-820x532.jpg'
         }
     }
+
+    componentDidMount = () => {
+        var specialContainer = ReactDOM.findDOMNode(this);
+        Dragula([specialContainer]);
+    }
+
     render() {
+        // return (
+        //     <div className='container'>
+        //         <div>Swap me around</div>
+        //         <div>Swap her around</div>
+        //         <div>Swap him around</div>
+        //         <div>Swap them around</div>
+        //         <div>Swap us around</div>
+        //         <div>Swap things around</div>
+        //         <div>Swap everything around</div>
+        //     </div>
+        // )
         return (
             <div id="dashboard-container">
-                <div id="tile-ribbon" className="row">
+                <div id="tile-ribbon" className="row specialContainer">
                     {
                         this.props.tiles.map((item, index) =>
-                            <div key={index} className="container tile-container col-sm-3">
-                                <DashboardTile title={item.title} content={item.content} />
+                            <div key={index} className=" container tile-container col-sm-3">
+                                <ChartTile />
+                                {/* <DashboardTile title={item.title} content={item.content} /> */}
                             </div>
                         )}
                 </div>
                 <div id="large-tiles" className="row col-sm-12" >
-                    <div className="container" ref={this.dragulaDecorator} >
-                        <LargeTile map={this.state.img} />
-                        <LargeTile map={this.state.img} />
-                        <LargeTile map={this.state.img} />
-                    </div>
+                    <div className="specialContainer" ><ChartTile /></div>
+                </div>
+
+                <div className="specialContainer" ><LargeTile map={this.state.img} /></div>
+                <div className="specialContainer" ><LargeTile map={this.state.img} /></div>
+
 
                 </div>
-            </div>
         )
-        dragulaDecorator = (componentBackingInstance) => {
-            if (componentBackingInstance) {
-              let options = { };
-              Dragula([componentBackingInstance], options);
-            }
-          };
+
     }
 }
 
