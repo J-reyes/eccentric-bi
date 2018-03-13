e764246df3a127fbc3cee331233dabd2fd479079
Patrick Truong <pattruong@gmail.com>
2018-03-13 10:24:28 -0700

    added chart-tile

--- src/components/redux/reducer/index.js
+++ src/components/redux/reducer/index.js
@@ -16,7 +16,38 @@ const initialState = {
         }
     ],
     register: { firstnameInput: '', lastnameInput: '', companyInput: '', email: '', passwordInput: '', confirmInput: ''}
+}
 
+const chartState = {
+    data: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+}
+
+const charts = (state = chartState, action) => {
+    return state;
 }
 
 const navbarFields = (state = initialState, action) => {
@@ -46,12 +77,12 @@ const loginPage = (state = initialState, action) => {
 const registrationForm = (state = initialState, action) => {
     switch(action.type) {
         case CREATE_ACCOUNT:
-        return {
+            return {
             ...state,
             register: action.payload
         }
         default:
-        return state;
+            return state;
     }
 }
 
@@ -59,7 +90,8 @@ const registrationForm = (state = initialState, action) => {
 const rootReducer = combineReducers({
     navbarFields,
     loginPage,
-    registrationForm
+    registrationForm,
+    charts
 })
 
 
