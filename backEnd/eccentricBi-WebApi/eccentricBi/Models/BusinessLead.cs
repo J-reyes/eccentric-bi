df6ec472e4df2df595ba4545f63ad77075bad608
Andrew Hwang <Andrew@Andrews-MBP.attlocal.net>
2018-04-02 11:30:59 -0700

    added new backend file directory with gitignore

---
+++ backEnd/eccentricBi-WebApi/eccentricBi/Models/BusinessLead.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace eccentricBi.Models
+{
+    public class BusinessLead
+    {
+        public BusinessLead()
+        {
+        }
+    }
+}
---
+++ backEnd/eccentricBi-WebApi/eccentricBi/Models/LeadContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+
+namespace eccentricBi.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class LeadContext : DbContext
+    {
+        public LeadContext()
+        {
+            
+        }
+
+        public LeadContext(DbContextOptions<LeadContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<BusinessLead> BusinessLeads { get; set; }
+
+
+    }
+
+
+}
